135e5198fc1704a174553f9e207f71bcf67ab38c
Jerry Bi <jerrybi0522@gmail.com>
2016-04-11 17:34:53 -0400

    Init commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// Different Data Types
+
+	// String
+	"Hello"
+
+	// Number
+	2, 3.5, 9000
+
+	// Boolean
+	true, false
+
+	// No Value
+	undefined
+
+// Comments
+
+	// This is a single line comment
+
+	/* This
+	is a
+	multi-line
+	comment */
+
+// Use console to print something
+
+	console.log("Printing something with console");
+
+// Make an alert
+
+	alert("This is an alert")
+
+// Variables
+
+	var string = "String";
+	var number = 9;
+	var isThisABoolean = true;
+
+// Arrays
+
+	["A", "B", "C"]
+	[1, 2, 3]
+
+	// Store arrays in variables
+	var letters = ["A", "B", "C"];
+
+	// Indexes
+		// "A" has the index of 0, "B" has the index of 1, and so on
+
+	// Put arrays in other arrays
+	var letters = ["A", "B", "C"];
+	var numbers = [1, 2, 3];
+
+		// Put variables in array
+		var pair = [letters, number];
+
+		// Access item in arrays by position
+		console.log(pair[0][0]);
+		// This will get the first item of the first array
